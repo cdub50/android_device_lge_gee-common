34d662b1c6c15b1fe3eacb360fe9d63d3f7c3e4c
Jake Weinstein <xboxfanj@msn.com>
2013-11-28 11:41:41 -0600

    Linaro changes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
@@ -83,12 +83,6 @@ TARGET_USES_ION := true
 TARGET_USES_OVERLAY := true
 TARGET_USES_SF_BYPASS := true
 TARGET_USES_C2D_COMPOSITON := true
-TARGET_DISPLAY_USE_RETIRE_FENCE := true
-
-BOARD_USES_QCOM_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
-TARGET_USES_QCOM_BSP := true
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
--- gee-common.mk
+++ gee-common.mk
@@ -270,3 +270,4 @@ PRODUCT_RUNTIMES += \
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+$(call inherit-product, device/linaro/common/common.mk)
